7442d7f34dfe52fc0f3b0651c675273e262e1543
clement91190 <clem.jambou@gmail.com>
2014-03-08 21:01:40 +0000

    ...progress

--- imgTranslate/urls.py
+++ imgTranslate/urls.py
@@ -2,10 +2,19 @@
 #from flask import request
 from imgTranslate import app
 import json
+from decaf.scripts.imagenet import DecafNet
+import PIL
+import numpy as np
 
+net = DecafNet('../imagenet_pretrained/imagenet.decafnet.epoch90', '../imagenet_pretrained/imagenet.decafnet.meta')
+img = PIL.Image.open('../image/apple.jpg')
+img  = np.array(img.getdata(), dtype='uint8').reshape(img.size[0], img.size[1], 3)
 
 # Worker functions
+
 @app.route('/analyse', methods=['POST', 'GET'])
 def analyse():
-    return json.dumps("tree")
+    scores = net.classify(img)
+    str =  "{}".format(net.top_k_prediction(scores, 5))
+    return json.dumps(str)
 
--- run_server.py
+++ run_server.py
@@ -3,6 +3,7 @@ from imgTranslate import app
 import imgTranslate.urls
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    #port = int(os.environ.get('PORT', 5000))
+    port = 5001
     debug = (port == 5000)
     app.run(host='0.0.0.0', port=port, debug=debug)
